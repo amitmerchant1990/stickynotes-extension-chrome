9f79837c37188627edb364523a0ecbf5b1a78e34
Amit Merchant <bullredeyes@gmail.com>
2014-05-17 21:05:11 +0530

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,48 @@
+var db = new PouchDB('stickyNotes');
+    var lastIndex = 1;
+    var remoteCouch = false;
+   
+    function addSticky(sticky){
+      
+      var sticky_note = {
+        _id: new Date().toISOString(),
+       content: sticky
+      };
+      db.put(sticky_note, function callback(err, result) {
+        if (!err) {
+          console.log('Successfully a sticky note!');
+        }
+      });
+      
+    }
+    
+    function fetchStickys(){
+      db.allDocs({include_docs: true}, function(err, response) {
+          console.log(response.rows);
+          var final_response = response.rows;
+          var i = 1;
+          for (var key in final_response) {
+            if (final_response.hasOwnProperty(key)) {
+              var sticky_content = final_response[key].doc.content;
+              $('#sticky_notes_area').append(i+". "+sticky_content+'<br>');
+            }
+            i++;
+            lastIndex = i;
+          }
+          
+      });
+    }
+    
+    $(document).ready(function(){
+      fetchStickys();
+      $('#sticky_notes_add').click(function(){
+        var content = $('#sticky_notes_text').val();
+        
+        addSticky(content);
+        
+        $('#sticky_notes_area').append(lastIndex+". "+content+'<br>');
+        lastIndex++;
+      });
+      
+      
+    });1
